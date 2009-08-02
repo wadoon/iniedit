6c3a508d4c87b8f33d29474725d3d3590068d30a
Alexander Weigl <alexweigl@gmail.com>
2009-08-02 07:17:00 +0200

    untested setup.py added

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='iniedit',
+      version='0.1',
+      description='edit ini files interactively, in batch or from python',
+      author='Alexander Weigl',
+      author_email='alexweigl@gmail.com',
+      url='http://areku.kilu.de',
+      download_url='',
+      scripts=[""],
+      py_modules=["IniEdit"}],
+      provides=["IniEdit(0.1)"],
+
+      classifiers=[ # see http://pypi.python.org/pypi?%3Aaction=list_classifiers for all !
+#        'Development Status :: 4 - Beta',
+#        'Environment :: Console',
+#        'Environment :: Web Environment',
+#        'Intended Audience :: End Users/Desktop',
+#        'Intended Audience :: Developers',
+#        'Intended Audience :: System Administrators',
+#        'License :: OSI Approved :: Python Software Foundation License',
+#        'Operating System :: MacOS :: MacOS X',
+#        'Operating System :: Microsoft :: Windows',
+#        'Operating System :: POSIX',
+#        'Programming Language :: Python',
+#        'Topic :: Communications :: Email',
+#        'Topic :: Office/Business',
+#        'Topic :: Software Development :: Bug Tracking',
+        ],      
+      long_description="""${long_desc}""",
+     )
